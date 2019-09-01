b20bb6102ad3d16675af3ba199631f41d334973c
James Steinbach <jdsteinbach@gmail.com>
2019-09-01 16:23:20 -0600

    theme, partial content

---
+++ src/js/index.js
@@ -0,0 +1,15 @@
+import { initialize } from 'reveal.js'
+import { initHighlightingOnLoad } from 'highlight.js'
+
+initialize({
+  controls: true,
+  progress: true,
+  history: true,
+  center: true,
+  width: 800,
+  height: 450,
+  transition: 'fade'
+});
+
+
+initHighlightingOnLoad()
